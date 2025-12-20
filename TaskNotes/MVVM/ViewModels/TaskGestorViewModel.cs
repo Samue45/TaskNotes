05e845e520968ef632c3a22cd26630e259f5ade8
Samu-Mind <samuelayllonsevilla86@gmail.com>
2025-12-20 09:06:41 +0100

    Btn delete added

--- TaskNotes/MVVM/ViewModels/TaskGestorViewModel.cs
+++ TaskNotes/MVVM/ViewModels/TaskGestorViewModel.cs
@@ -2,7 +2,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
-using TaskNotes.MVVM.Models; // Asegúrate de que este using coincida con tu modelo TaskItem
+using TaskNotes.MVVM.Models; 
 
 namespace TaskNotes.MVVM.ViewModels;
 
